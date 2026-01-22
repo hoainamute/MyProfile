ea66861b3438b6257d2e8c3ac239770600543df5
namnh <nam.nguyen@digime.asia>
2026-01-22 21:37:28 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Sử dụng base: './' để đảm bảo website hoạt động dù bạn đặt tên repo là gì
-  base: './',
+  base: '/MyProfile/',
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    sourcemap: false
   }
 });
